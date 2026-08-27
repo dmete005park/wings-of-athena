517a4ea180bc4f09352b77ea7f9238f2cade2984
dmete005park <david@calvinpark.net>
2026-08-27 04:13:24 -0400

    feat(math): export Sprint 0 engine surface

---
+++ packages/math-engine/src/index.ts
@@ -0,0 +1,15 @@
+export * from './types';
+export * from './validation';
+export * from './electorate';
+export * from './raceRules';
+export * from './victory';
+export * from './universe';
+export * from './outreach';
+export * from './objectives';
+export * from './capacity';
+export * from './pacing';
+export * from './alerts';
+export * from './metricRegistry';
+export * from './assumptionRegistry';
+
+export const MATH_ENGINE_VERSION = '0.2.0-alpha.1';
